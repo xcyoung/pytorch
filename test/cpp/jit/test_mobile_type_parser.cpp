3db80902f0d6467458a50cdc186d23ae4ae6976d
Martin Yuan <iseeyuan@yahoo.com>
2019-12-06 17:31:00 -0800

    Type parser
    ghstack-source-id: 98dcc285cb6150b155109947b58013bfeee661a3
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/30391

---
+++ test/cpp/jit/test_mobile_type_parser.cpp
@@ -0,0 +1,44 @@
+#include "test/cpp/jit/test_base.h"
+#include <gtest/gtest.h>
+
+namespace c10 {
+//std::string serializeType(const Type &t);
+TypePtr parseType(const std::string& pythonStr);
+}
+
+namespace torch {
+namespace jit {
+void testMobileTypeParser() {
+  std::string empty_ps("");
+  EXPECT_ANY_THROW(c10::parseType(empty_ps));
+
+  std::string int_ps("int");
+  auto int_tp = c10::parseType(int_ps);
+  std::string int_tps = int_tp->python_str();
+  ASSERT_EQ(int_ps, int_tps);
+
+  std::string tuple_ps("Tuple[str, Optional[float], Dict[str, List[Tensor]], int]");
+  auto tuple_tp = c10::parseType(tuple_ps);
+  std::string tuple_tps = tuple_tp->python_str();
+  ASSERT_EQ(tuple_ps, tuple_tps);
+
+  std::string tuple_space_ps("Tuple[  str, Optional[float], Dict[str, List[Tensor ]]  , int]");
+  auto tuple_space_tp = c10::parseType(tuple_space_ps);
+  // tuple_space_tps should not have weird white spaces
+  std::string tuple_space_tps = tuple_space_tp->python_str();
+  ASSERT_EQ(tuple_ps, tuple_space_tps);
+
+  std::string typo_token("List[tensor]");
+  EXPECT_ANY_THROW(c10::parseType(typo_token));
+
+  std::string mismatch1("List[Tensor");
+  EXPECT_ANY_THROW(c10::parseType(mismatch1));
+
+  std::string mismatch2("List[[Tensor]");
+  EXPECT_ANY_THROW(c10::parseType(mismatch2));
+
+  std::string mismatch3("Dict[Tensor]");
+  EXPECT_ANY_THROW(c10::parseType(mismatch2));
+}
+} // namespace torch
+} // namespace jit
--- test/cpp/jit/tests.h
+++ test/cpp/jit/tests.h
@@ -74,7 +74,8 @@ namespace jit {
   _(LiteInterpreterInline)             \
   _(LiteInterpreterTuple)              \
   _(LiteInterpreterPrimOverload)       \
-  _(CommonAncestor)
+  _(CommonAncestor)                    \
+  _(MobileTypeParser)
 
 #define TH_FORALL_TESTS_CUDA(_) \
   _(ArgumentSpec)               \
